a47f64e7795be43486e7dd0d9041271916745358
jlxw48 <jlxw48@gmail.com>
2021-10-20 22:38:34 +0800

    random

--- load-testing-1/index.js
+++ load-testing-1/index.js
@@ -1,8 +1,36 @@
 const express = require( 'express' );
+const axios = require("axios").default;
 
 const app = express();
 app.use( express.json() );
 const port = process.env.PORT || 3000;
+
+app.route( "/load-1/questions" )
+    .get( ( req, res ) => {
+    console.log("http://questions:3000/api/questions")
+        axios.get( 'http://questions:3000' ) // this is not routing to port 3000
+            .then( ( response ) => {
+                // console.log(response.data);
+                res.json( response.data );
+            } )
+            .catch( ( error ) => {
+                console.log( error );
+                res.json( { err_msg: "error fetching all photos" } );
+            } );
+    } );
+
+app.route( "/load-1/load-2" )
+    .get( ( req, res ) => {
+        fetch("http://load-2:3000")
+            .then((response) => {
+                res.json("reached load 2");
+            })
+            .catch((err) => {
+                res.json("failed to reach load 2");
+            }
+
+            )
+    } );
 
 app.route( "/load-1/" )
     .get( ( req, res ) => {
