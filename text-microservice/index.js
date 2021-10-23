25a35c18193210f4d7b96300538bcd480ff9588f
candyhy <candiceyhy@gmail.com>
2021-10-23 13:22:00 +0800

    Update redis pubsub system

--- text-microservice/index.js
+++ text-microservice/index.js
@@ -3,75 +3,78 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const bodyParser = require('body-parser');
+
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: false}));
 
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require('socket.io');
-
-var redis_endpoint = process.env.REDIS_ENDPOINT;
-var redis_pw = process.env.REDIS_PASSWORD;
+const { Server } = require("socket.io");
+const { createAdapter } = require("@socket.io/redis-adapter");
 
 const io = new Server(server, {
+     path: "/editor/create",
      cors: {
          origin: "*",
          methods: ["GET", "POST"]
      }
-});
+ });
 
+var redis_endpoint = process.env.REDIS_ENDPOINT;
+var redis_pw = process.env.REDIS_PASSWORD;
 
-const redis = require("redis");
-const client = redis.createClient({
-	host: redis_endpoint,
-	port:6379,
-	auth_pass: redis_pw
-});
+var redis = require("redis"),
 
-client.on("connect", () => console.log("Connected to Redis"));
-client.on("error", function (error) {
+  subClient = redis.createClient({
+  	host: redis_endpoint,
+  	port:6379,
+  	auth_pass: redis_pw
+  }),
+
+  pubClient = redis.createClient({
+    host: redis_endpoint,
+    port:6379,
+    auth_pass: redis_pw
+  });
+
+io.adapter(createAdapter(pubClient, subClient));
+
+pubClient.on("connect", () => console.log("pubClient connected to Redis"));
+
+pubClient.on("error", function (error) {
   console.error(error);
 });
 
+app.get("/editor/get_text", (req, res) => {
+	const sessionId = req.query.interviewId;
 
-app.get("/editor/new-session", (req, res) => {
-	const sessionId = req.query.sessionId;
-	const qnsId = req.query.questionId;
-  console.log(req.query);
-  if (!sessionId) {
-    res.status(400).json("No parameters");
-  }
-  client.get(sessionId, (err, response) => {
+  pubClient.get(sessionId, (err, response) => {
+
     if (response === null) {
-      client.set(sessionId, qnsId);
-  	  res.sendFile(__dirname + '/page.html');
 
-     /* res.status(200).json({
-      	status: "success",
+     res.status(400).json({
+      	status: "failure",
       	data: {
-      		message: "New session created"
+      		message: "No prior text history"
       	}
-
-      });*/
+      });
     } else {
-    	res.sendFile(__dirname + '/page.html');
-
-    	/*res.status(200).json({
+      console.log(response);
+    	res.status(200).json({
     		status: "success",
     		data: {
-    			message: "Session in progress"
+    			message: response
     		}
-    	});*/
+    	});
     }
   });
 });
 
-
-
 app.get('/editor/find-session', (req, res) => {
-  const sessionId = req.query.sessionId;
-  client.get(sessionId, (err, response) => {
+  const sessionId = req.query.interviewId;
+  pubClient.get(sessionId, (err, response) => {
   	if (response === null) {
   		res.status(400).json({
       	status: "failure",
@@ -91,8 +94,8 @@ app.get('/editor/find-session', (req, res) => {
 });
 
 app.get("/editor/end-session", (req, res) => {
-  const sessionId = req.query.sessionId;
-  client.get(sessionId, (err, response) => {
+  const sessionId = req.query.interviewId;
+  pubClient.get(sessionId, (err, response) => {
     if (response === null) {
       res.status(400).json({
       	status: "failure",
@@ -102,7 +105,7 @@ app.get("/editor/end-session", (req, res) => {
 
       });
     } else {
-    	client.del(sessionId, (err, response) => {
+    	pubClient.del(sessionId, (err, response) => {
 		    console.log("Deleted ", sessionId);
 		    res.status(200).json({
 		    	status: "success",
@@ -115,9 +118,20 @@ app.get("/editor/end-session", (req, res) => {
   });  
 });
 
+const saveText = (newText) => {
+  const sessionId = newText.interviewId;
+  const text = newText.text;
+  pubClient.set(sessionId, text, redis.print);
+};
+
 io.on("connection", socket => {
+  console.log("connected");
+
 	socket.on("message", message => {
-		io.emit(message.sessionId, message.text);
+    console.log(message);
+    saveText(message);
+    const interviewId = message.interviewId;
+    io.emit(message.interviewId, message.text);
 	});
 });
 
